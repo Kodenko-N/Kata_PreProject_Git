e87f8f749fdd17d70305992b877a24b0fe8c5553
Kodenko-N <kodenko.n@gmail.com>
2025-01-27 11:17:43 +0300

    Pre-version. DB connection every time

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,24 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/kn_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "RootPassword1";
+    public static Connection conn;
 
-    public static Statement getConnection(String url, String user, String psw) throws SQLException {
-        Connection connection = DriverManager.getConnection(url, user, psw);
-        //System.out.println("Connected to " + url);
-        Statement stmt = connection.createStatement();
-        return stmt;
+    public static Connection getConnection() throws SQLException {
+        try {
+            System.out.println("Connecting to " + URL);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("DB connection failed");
+            System.out.println("Please choose an option: ");
+            System.out.println(" 1: Close program");
+            System.out.println(" Any other key: try again connect to:" + URL);
+            Scanner sc = new Scanner(System.in);
+            String input = sc.next();
+            while (!input.equals("1")) {
+                getConnection();
+                input = sc.nextLine();
+            }
+            System.out.println("Program finished");
+            System.exit(0);
+            return null;
+        }
     }
-
-//    public static void closeConnection(Connection connection) {
-//        if (connection != null) {
-//            connection.close();
-//            System.out.println("Connection closed");
-//        } else {
-//            System.out.println("DB null link during closure");
-//        }
-//    }
 }
 
 
