2b74b948582e12d7cf4f65a208d3e8d4f5650516
Kodenko-N <kodenko.n@gmail.com>
2025-01-29 11:42:47 +0300

    Task solution via Hibernate. v.1.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
@@ -7,10 +8,11 @@ import jm.task.core.jdbc.util.Util;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Programm had been started");
+        System.out.println("Program had been started");
         UserService userService = new UserServiceImpl();
         userService.dropUsersTable();
         userService.createUsersTable();
@@ -18,7 +20,10 @@ public class Main {
         userService.saveUser("Ivan2", "Petrov2", (byte) 10);
         userService.saveUser("Василий", "Иванов", (byte) 15);
         userService.saveUser("Елена", "Прекрасная", (byte) 20);
-        userService.getAllUsers();
+        List<User> usersList= userService.getAllUsers();
+        for (User u : usersList) {
+            System.out.println(u);
+        }
         userService.removeUserById((long) 2);
         userService.cleanUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,119 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
-
-    }
-
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String createTable = "CREATE TABLE IF NOT EXISTS `USERS` (" +
+                    "  `Id` BIGINT NOT NULL AUTO_INCREMENT," +
+                    "  `Name` VARCHAR(45) NOT NULL," +
+                    "  `LastName` VARCHAR(45) NOT NULL," +
+                    "  `Age` TINYINT NOT NULL," +
+                    "  PRIMARY KEY (`Id`))";
+           session.createSQLQuery(createTable).addEntity(User.class).executeUpdate();
 
+            transaction.commit();
+            System.out.println("Table has been created by Hibernate");
+        } catch (Exception e) {
+            System.out.println("Exception occurred while Hibernate creating a table");
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String tableDelete = "DROP TABLE IF EXISTS USERS";
+            session.createSQLQuery(tableDelete).addEntity(User.class).executeUpdate();
+            transaction.commit();
+            System.out.println("Table has been dropped by Hibernate");
+        } catch (Exception e) {
+            System.out.println("Exception occurred while Hibernate dropping a table");
+            System.out.println(e.getMessage());
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("User " + name + " has been saved by Hibernate");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Exception occurred while Hibernate saving user " + name);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User userToDelite = (User) session.load(User.class, id);
+            session.delete(userToDelite);
+            transaction.commit();
+            System.out.println("User " + userToDelite.getName() + " has been removed by Hibernate");
 
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Exception occured while removing user by Hibernate, ID = " + id);
+        }
     }
+
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List <User> users = new ArrayList<User>();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            users = session.createCriteria(User.class).list();
+            transaction.commit();
+            System.out.println("User list created by Hibernate");
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred while getting User list by Hibernate");
+            System.out.println(e.getMessage());
+        }
+        return users;
     }
 
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE User").executeUpdate();
+            transaction.commit();
+            System.out.println("The table cleared by Hibernate");
+        } catch (Exception e) {
+            System.out.println("Exception occurred during Table clear by Hibernate");
+            System.out.println(e.getMessage());
+        }
     }
 }
